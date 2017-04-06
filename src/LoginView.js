c384d4e31fbb2bb583aca2b826ce35238d6d1c6c
William Montiel <william.montiel@creardigital.com>
2017-04-06 15:04:02 -0500

    Firebase Auth

--- src/LoginView.js
+++ src/LoginView.js
@@ -32,21 +32,23 @@ const { GoogleAuthProvider } = firebase.auth;
 const firebaseAuth = firebase.auth();
 
 export default class HomeView extends Component {
-    componentDidMount() {
+    componentWillMount() {
         this._hasPlayServices();
     }
 
-    authenticateUser(accessToken) {
-        console.warn("LALA", accessToken);
-        const credential = GoogleAuthProvider.credential(accessToken)
-        firebaseAuth.signInWithCredential(credential).then(function (user) {
-            console.warn("Sign In Success", user);
-            var currentUser = user;
-            // Merge prevUser and currentUser accounts and data
-            // ...
+    state = {
+        user: null
+    }
+
+    authenticateUser = () => {
+        const credential = GoogleAuthProvider.credential(this.state.user.idToken)
+        firebaseAuth.signInWithCredential(credential).then((user) => {
+            this.goToHome()
+            //console.warn("FIREBASE", user);
         }, function (error) {
             console.warn("Sign In Error", error);
         });
+        
     }
 
     _hasPlayServices() {
@@ -54,64 +56,51 @@ export default class HomeView extends Component {
             // play services are available. can now configure library
             this._configure()
         })
-            .catch((err) => {
-                console.warn("Play services error", err.code, err.message);
-            })
+        .catch((err) => {
+            console.warn("Play services error", err.code, err.message);
+        })
     }
 
     _configure() {
         GoogleSignin.configure({
-            //webClientId: '632962718188-37tagq7p2jql3r75uf22hc3ko8gun9la.apps.googleusercontent.com',
+            webClientId: '632962718188-uj14nf9no745am3q8j8b410e08beo0m8.apps.googleusercontent.com',
         })
-            .then(() => {
-                this._currentUserAsync();
-            });
+        .then(() => {
+            this._currentUserAsync();
+        });
     }
 
     _currentUserAsync() {
         GoogleSignin.currentUserAsync().then((user) => {
             this.setState({ user: user });
+            if (this.state.user != null) {
+                this.authenticateUser();
+            }
         }).done();
     }
 
     _signIn() {
-        if (this.state.user == null) {
-            GoogleSignin.signIn()
-                .then((user) => {
-                    this.setState({ user: user });
-                    this._getAccessToken()
-                })
-                .catch((err) => {
-                    console.warn('WRONG SIGNIN', err);
-                })
-                .done();
-        } else {
-            //this.goToHome();
-        }
+        GoogleSignin.signIn()
+            .then((user) => {
+                this.setState({ user: user });
+                console.warn('SIGN IN', user);
+                this.authenticateUser()
+            })
+            .catch((err) => {
+                console.warn('WRONG SIGNIN', err);
+            })
+            .done();
     }
 
     _signOut() {
         GoogleSignin.signOut()
             .then(() => {
                 this.setState({ user: null });
-                //this.goToHome();
                 console.warn('SIGN OUT', this.state.user);
             })
             .catch((err) => {
                 console.warn('WRONG OUT', err);
             });
-    }
-
-    _getAccessToken() {
-        GoogleSignin.getAccessToken()
-            .then((token) => {
-                console.warn("TOKEN", token);
-                this.authenticateUser(token)
-            })
-            .catch((err) => {
-                console.warn("WRONG TOKEN", err);
-            })
-            .done();
     }
 
     _revokeAccess() {
@@ -129,23 +118,26 @@ export default class HomeView extends Component {
     }
 
     render() {
-        let googleButton;
-        //console.warn("LALA", this.state.user);
-
+        let username = null;
+        let googleSign = this._signIn;
+        let googleSignText = "Log In";
+        if (this.state.user != null) {
+            username = this.state.user.name;
+            googleSign = this._signOut;
+            googleSignText = "Log Out";
+        }
 
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
                     Bienvenidos a Platzi Music
                 </Text>
-                <GoogleSigninButton
-                    style={{ width: 48, height: 48 }}
-                    size={GoogleSigninButton.Size.Icon}
-                    color={GoogleSigninButton.Color.Dark}
-                    onPress={this._signIn.bind(this)} />
+                <Text style={styles.welcome}>
+                    {username}           
+                </Text>
 
-                <Icon.Button name="google" backgroundColor="#ddd" onPress={this._signOut.bind(this)}>
-                    <Text style={{ fontFamily: 'Arial', fontSize: 15 }}>Log out</Text>
+                <Icon.Button name="google" backgroundColor="red" onPress={googleSign.bind(this)}>
+                    <Text style={{ fontFamily: 'Arial', color: 'white', fontSize: 15 }}>{googleSignText}</Text>
                 </Icon.Button>
 
             </View>
